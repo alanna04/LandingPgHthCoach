98a589eb4ccb9f25aa3affe931c3522b20c7744e
Al <t_alanna@msn.com>
2021-06-19 15:43:20 -0400

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Hi we just loaded the app')
+console.log(2+2);
+console.log('Alanna ' + 'Poppi')
+
+function printName(first, last) {
+    console.log (first + ' ' + last)
+    alert(first + ' ' + last)
+}
